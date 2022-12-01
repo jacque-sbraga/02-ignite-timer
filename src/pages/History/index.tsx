4e7a99532953b760480c9934e4d3ee226933889b
Jacqueline Braga <jaquelinesantosbraga@gmail.com>
2022-12-01 16:54:13 -0300

    feito a página de histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,56 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Limpar caixa d' água</td>
+              <td>60 minutos</td>
+              <td>Há dois meses atrás</td>
+              <td>
+                <Status statusColor='green' >Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Limpar caixa d' água</td>
+              <td>60 minutos</td>
+              <td>Há dois meses atrás</td>
+              <td>
+                <Status statusColor='green' >Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Limpar caixa d' água</td>
+              <td>60 minutos</td>
+              <td>Há dois meses atrás</td>
+              <td>
+                <Status statusColor='green' >Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Limpar caixa d' água</td>
+              <td>60 minutos</td>
+              <td>Há dois meses atrás</td>
+              <td>
+                <Status statusColor='green' >Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,23 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto1" />
+            <option value="Projeto2" />
+            <option value="Projeto3" />
+            <option value="Projeto4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            min={5}
+            max={60}
+            step={5}
           />
           <span>minutos.</span>
         </FormContainer>
